ac3795415b4efd1b922ff293fdbccd3a374f0ae7
♿ <biznes@2.pl>
2022-03-01 22:04:58 +0100

    Update tripInfo.js

--- functions/tripInfo.js
+++ functions/tripInfo.js
@@ -3,12 +3,14 @@ export const onRequestGet = async({ request }) => {
     const tripId = searchParams.get('trip');
     const vehicleId = searchParams.get('vehicle');
 
+    let now = Date.now();
     const trip = tripId ? await fetch(`https://new-api.domeqalt.repl.co/get/trip?id=${tripId}`).then(res => res.json()) : null;
     const vehicle = vehicleId ? await fetch(`https://new-api.domeqalt.repl.co/get/vehicle?veh=${vehicleId}`).then(res => res.json()) : null;
 
     return new Response(JSON.stringify({
         success: true,
         trip: trip?.success ? trip : null,
-        vehicle: vehicle?.success ? vehicle : null
+        vehicle: vehicle?.success ? vehicle : null,
+        time: Date.now() - now
     }));
 };
