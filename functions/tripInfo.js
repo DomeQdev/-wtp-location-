90b2d95380a70d7f63e85dea39558a49c9e1f0a0
♿ <biznes@2.pl>
2022-03-03 17:45:19 +0100

    small changes

--- functions/tripInfo.js
+++ functions/tripInfo.js
@@ -1,4 +1,4 @@
-export const onRequestGet = async({ request }) => {
+export const onRequestGet = async ({ request }) => {
     const { searchParams } = new URL(request.url);
     const tripId = searchParams.get('trip');
     const vehicleId = searchParams.get('vehicle');
@@ -8,8 +8,26 @@ export const onRequestGet = async({ request }) => {
 
     return new Response(JSON.stringify({
         success,
-        trip,
-        vehicle,
+        trip: {
+            line: trip[0],
+            alerts: trip[1],
+            color: trip[2],
+            headsign: trip[3],
+            shapes: trip[4],
+            stops: trip[5]
+        },
+        vehicle: {
+            brand: vehicle[0],
+            model: vehicle[1],
+            prodYear: vehicle[2],
+            type: vehicle[3],
+            registration: vehicle[4],
+            tab: vehicle[5],
+            carrier: vehicle[6],
+            depot: vehicle[7],
+            features: vehicle[8],
+            description: vehicle[9]
+        },
         time: Date.now() - now
     }));
 };
