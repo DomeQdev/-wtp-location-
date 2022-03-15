e2af81d8be90eb26c3c22caa80230983a1b512a4
♿ <biznes@2.pl>
2022-03-15 17:05:37 +0100

    naprawić skm

--- functions/loadVehicles.js
+++ functions/loadVehicles.js
@@ -1,6 +1,6 @@
-export const onRequestGet = async() => {
+export const onRequestGet = async () => {
     let data = await fetch("https://mkuran.pl/gtfs/warsaw/vehicles.json").then(res => res.json()).catch(() => null);
-    let skm = await fetch("https://new-api.domeqalt.repl.co/get/skmPredict").then(res => res.json()).catch(() => null);
+    let skm = await getSKM();
     if (!data || !data.positions) return new Response("[]");
     return new Response(JSON.stringify(data.positions.map(x => {
         let trip = x.trip_id.split("/");
@@ -17,3 +17,20 @@ export const onRequestGet = async() => {
         }
     }).concat(skm)));
 };
+
+
+async function getSKM() {
+    let data = await fetch("https://new-api.domeqalt.repl.co/get/skmPredict").then(res => res.json()).catch(() => null);
+    if (!data) return [];
+    return data.map(skm => ({
+        line: skm.line,
+        type: "skm",
+        location: skm.location,
+        deg: 0,
+        previous: skm.previousLocation,
+        brigade: "0",
+        tab: skm.trip.replace(/\//g, "."),
+        lastPing: Date.now(),
+        trip: skm.trip
+    }))
+}
