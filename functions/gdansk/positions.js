27b59977e259bf7f334ee74780a9d62c5037da8b
♿ <biznes@2.pl>
2022-03-26 22:00:42 +0100

    Update positions.js

--- functions/gdansk/positions.js
+++ functions/gdansk/positions.js
@@ -2,21 +2,18 @@ export const onRequestGet = async () => {
     let response = await fetch("https://ckan2.multimediagdansk.pl/gpsPositions?v=2").then(res => res.json()).catch(() => null);
     if (!response) return new Response("[]");
 
-    return new Response(JSON.stringify(response.vehicles.map(vehicle => {
-        let start = new Date(vehicle.scheduledTripStartTime);
-        return {
-            brigade: vehicle.vehicleService.split("-")[1],
-            deg: vehicle.direction,
-            lastPing: new Date(vehicle.generated).getTime(),
-            line: vehicle.routeShortName,
-            location: [vehicle.lat, vehicle.lon],
-            tab: vehicle.vehicleCode,
-            tripId: vehicle.tripId,
-            trip: `${vehicle.routeShortName}${start.getFullYear()}${(start.getMonth() + 1).zeroPad()}${start.getDate().zeroPad()}${start.getHours().zeroPad()}${start.getMinutes().zeroPad()}`,
-            type: vehicle.routeShortName < 20 ? "tram" : "bus",
-            delay: vehicle.delay || 0
-        }
-    })));
+    return new Response(JSON.stringify(response.vehicles.map(vehicle => ({
+        brigade: vehicle.vehicleService.split("-")[1],
+        deg: vehicle.direction,
+        lastPing: new Date(vehicle.generated).getTime(),
+        line: vehicle.routeShortName,
+        location: [vehicle.lat, vehicle.lon],
+        tab: vehicle.vehicleCode,
+        tripId: vehicle.tripId,
+        headsign: vehicle.headsign,
+        type: vehicle.routeShortName < 20 ? "tram" : "bus",
+        delay: vehicle.delay || 0
+    }))));
 };
 
 Number.prototype.zeroPad = function () {
