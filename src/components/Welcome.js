042fa6a89aee78e56e9d3c1f24c2c705de5f54cb
♿ <biznes@2.pl>
2022-03-26 20:53:26 +0100

    Welcome

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,12 +2,19 @@ import { Box, FormControl, Button, RadioGroup, FormControlLabel, Radio } from "@
 import { NavigateNext, NavigateBefore } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Toggle } from "./misc/Switch";
 
 export default () => {
     const navigate = useNavigate();
     const [page, setPage] = useState(0);
 
-    const [city, setCity] = useState("");
+    const [city, setCity] = useState(null);
+    const [mapStyle, setMapStyle] = useState("gmaps");
+    const [customMapStyle, setCustomMapStyle] = useState(null);
+    const [grouping, setGrouping] = useState(false);
+    const [showBrigade, setShowBrigade] = useState(false);
+    const [showVehicleInfo, setShowVehicleInfo] = useState(false);
 
     useEffect(() => {
         if (localStorage?.city) return navigate(`/${localStorage?.city}`);
@@ -27,8 +34,45 @@ export default () => {
                 </RadioGroup>
             </FormControl>
         </>}
-        {page === 1 && <></>}
-        {page === 2 && <></>}
+        {page === 1 && <>
+            <h2>Wybierz styl mapy:</h2>
+            <FormControl>
+                <RadioGroup value={mapStyle} onChange={({ target }) => {
+                    if (target.value === "custom") {
+                        let val = prompt("Wpisz link do własnej mapy.\n\nPrzykład: https://moja_mapa.org/{z}/{x}/{y}.png");
+                        if (!val || !val.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)/g)) return toast.error("Niepoprawny link do mapy.");
+                        setCustomMapStyle(val);
+                    }
+                    setMapStyle(target.value);
+                }}>
+                    <FormControlLabel value="gmaps" control={<Radio />} label="Google Maps" />
+                    <FormControlLabel value="gsat" control={<Radio />} label="Google Satelita" />
+                    <FormControlLabel value="osm" control={<Radio />} label="OpenStreetMap" />
+                    <FormControlLabel value="mapbox" control={<Radio />} label="Mapbox" />
+                    <FormControlLabel value="mapstr" control={<Radio />} label="Mapbox Streets" />
+                    <FormControlLabel value="mapnav" control={<Radio />} label="Mapbox Navigation" />
+                    <FormControlLabel value="mapsat" control={<Radio />} label="Mapbox Satelita" />
+                    <FormControlLabel value="custom" control={<Radio />} label={mapStyle === "custom" && customMapStyle ? customMapStyle : "Własna mapa"} />
+                </RadioGroup>
+            </FormControl>
+        </>}
+        {page === 2 && <>
+            <h2>Już prawie!</h2>
+            <FormControlLabel
+                control={<Toggle sx={{ m: 1 }} checked={grouping} onChange={() => setGrouping(!grouping)} />}
+                label="Grupowanie pojazdów zamiast ich ukrywania"
+            />
+            <br />
+            <FormControlLabel
+                control={<Toggle sx={{ m: 1 }} checked={showBrigade} onChange={() => setShowBrigade(!showBrigade)} />}
+                label="Pokazuj brygady pojazdów"
+            />
+            <br />
+            <FormControlLabel
+                control={<Toggle sx={{ m: 1 }} checked={showVehicleInfo} onChange={() => setShowVehicleInfo(!showVehicleInfo)} />}
+                label="Pokazuj tylko informacje o pojeździe"
+            />
+        </>}
 
         <div style={{ padding: "30px" }}>
             <Button variant="contained" startIcon={<NavigateBefore />} disabled={page === 0} onClick={() => setPage(page - 1)}>
@@ -43,6 +87,11 @@ export default () => {
 
     function save() {
         localStorage.setItem("city", city);
+        localStorage.setItem("mapStyle", mapStyle);
+        localStorage.setItem("customMapStyle", mapStyle === "custom" ? customMapStyle : null);
+        localStorage.setItem("grouping", grouping);
+        localStorage.setItem("showBrigade", showBrigade);
+        localStorage.setItem("showVehicleInfo", showVehicleInfo);
         navigate(`/${city}`);
     }
 };
