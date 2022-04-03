b7a3a164513b91a9f116e183b2f46633ef029d2a
♿ <biznes@2.pl>
2022-04-03 23:04:55 +0200

    Update ActiveStop.js

--- src/components/city/routes/ActiveStop.js
+++ src/components/city/routes/ActiveStop.js
@@ -5,6 +5,7 @@ import { useMap } from "react-leaflet";
 import { List, ListItem, ListItemText, ListItemAvatar, Divider } from "@mui/material";
 import { DirectionsBus, Tram, DirectionsTransit, DirectionsRailway, Subway, Train, AirportShuttle } from '@mui/icons-material';
 import VehicleMarker from "./VehicleMarker";
+import { toast } from "react-toastify";
 
 const types = {
     bus: <DirectionsBus style={{ height: "20px", width: "20px" }} />,
@@ -26,7 +27,10 @@ export default ({ city, vehicles }) => {
         fetch(`/api/${city}/stop?id=${id}`).then(res => res.json()).then(res => {
             setStop(res);
             if(res?.location) map.setView(res.location, 17);
-        }).catch(() => null);
+        }).catch(() => {
+            toast.error("Nie znaleziono takiego przystanku");
+            navigate(`/${city}`);
+        });
     }, []);
 
     const dep = departures?.map(dep => {
@@ -45,7 +49,7 @@ export default ({ city, vehicles }) => {
             snapPoints={({ maxHeight }) => [maxHeight / 4, maxHeight * 0.6, maxHeight - 40]}
         >
             <List>
-                {dep ? dep?.map((departure, i) => (
+                {dep?.length ? dep?.map((departure, i) => (
                     <ListItem button={!!departure?.vehicle} key={departure.trip} onClick={() => map.setView(vehicles.find(x => x.tab === departure.vehicle && x.type === departure.type)?.location || location, 17)}>
                         <ListItemAvatar><b style={{ color: "white", backgroundColor: departure?.color, borderRadius: "25px", padding: "5px", paddingLeft: "10px", paddingRight: "10px", display: "inline-flex", alignItems: "center" }}>{types[departure?.type]}&nbsp;{departure?.line}</b></ListItemAvatar>
                         <ListItemText>
@@ -59,7 +63,7 @@ export default ({ city, vehicles }) => {
                             </div>
                         </ListItemText>
                     </ListItem>
-                )).reduce((prev, curr) => [prev, <Divider variant="inset" component="li" key={Math.random()} sx={{ backgroundColor: "#DCCDCD", marginLeft: "15px", marginRight: "15px" }} />, curr]) : <h1 style={{ textAlign: "center" }}>-</h1>}
+                )).reduce((prev, curr) => [prev, <Divider variant="inset" component="li" key={Math.random()} sx={{ backgroundColor: "#DCCDCD", marginLeft: "15px", marginRight: "15px" }} />, curr]) : <h3 style={{ textAlign: "center" }}>Brak odjazdów w najbliższym czasie.</h3>}
             </List>
         </BottomSheet>
     </>;
