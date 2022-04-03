0512631ad20a9883002ecda579447ca4cbb10db7
♿ <biznes@2.pl>
2022-04-03 22:08:02 +0200

    Update ActiveStop.js

--- src/components/city/routes/ActiveStop.js
+++ src/components/city/routes/ActiveStop.js
@@ -1,19 +1,40 @@
 import { BottomSheet } from "react-spring-bottom-sheet";
 import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { useMap } from "react-leaflet";
+import { List, ListItem, ListItemText, ListItemAvatar, Divider, Avatar } from "@mui/material";
+import { DirectionsBus, Tram, DirectionsTransit, DirectionsRailway, Subway, Train } from '@mui/icons-material';
 import VehicleMarker from "./VehicleMarker";
+
+const types = {
+    bus: <DirectionsBus style={{ height: "20px", width: "20px" }} />,
+    tram: <Tram style={{ height: "20px", width: "20px" }} />,
+    metro: <Subway style={{ height: "20px", width: "20px" }} />,
+    wkd: <DirectionsRailway style={{ height: "20px", width: "20px" }} />,
+    skm: <Train style={{ height: "20px", width: "20px" }} />,
+    km: <DirectionsTransit style={{ height: "20px", width: "20px" }} />
+};
 
 export default ({ city, vehicles }) => {
     const { id } = useParams();
     const navigate = useNavigate();
+    const map = useMap();
     const [{ departures, name, location }, setStop] = useState([]);
 
     useEffect(() => {
-        fetch(`/api/${city}/stop?id=${id}`).then(res => res.json()).then(setStop).catch(() => null);
+        fetch(`/api/${city}/stop?id=${id}`).then(res => res.json()).then(res => {
+            setStop(res);
+            if(res?.location) map.setView(res.location, 17);
+        }).catch(() => null);
     }, []);
 
+    const dep = departures?.map(dep => {
+        dep.vehicle = vehicles.find(x => x.line === dep.line && x.brigade === dep.brigade);
+        return dep;
+    });
+
     return <>
-        {departures?.filter(departure => vehicles.find(x => x.tab === departure.vehicle && x.type === departure.type)).map(departure => <VehicleMarker vehicle={vehicles.find(x => x.tab === departure.vehicle && x.type === departure.type)} key={departure.trip || departure.tab} />)}
+        {dep?.filter(x => x?.vehicle).map(departure => <VehicleMarker vehicle={departure?.vehicle} key={departure.trip || departure.tab} />)}
         <BottomSheet
             open={true}
             onDismiss={() => navigate("/")}
@@ -22,9 +43,23 @@ export default ({ city, vehicles }) => {
             header={<><b>{name}</b></>}
             snapPoints={({ maxHeight }) => [maxHeight / 4, maxHeight * 0.6, maxHeight - 40]}
         >
-            {departures?.map(departure => <div key={departure.trip || departure.tab}>
-                {JSON.stringify(departure)}
-            </div>)}
+            <List>
+                {dep?.map((departure, i) => (
+                    <ListItem button={!!departure?.vehicle} disabled={!departure?.vehicle} key={departure.trip} onClick={() => map.setView(vehicles.find(x => x.tab === departure.vehicle && x.type === departure.type)?.location || location, 17)}>
+                        <ListItemAvatar><b style={{ color: "white", backgroundColor: departure?.vehicle ? departure?.color : "#ADADAD", borderRadius: "25px", padding: "5px", paddingLeft: "10px", paddingRight: "10px", display: "inline-flex", alignItems: "center" }}>{types[departure?.type]}&nbsp;{departure?.line}</b></ListItemAvatar>
+                        <ListItemText>
+                            <div style={{ float: "left", textAlign: "left", color: departure.realTime < Date.now() ? "#ADADAD" : null }}>
+                                <div style={{ display: "inline-flex", alignItems: "center" }}>
+                                    {departure?.headsign ? <>&nbsp;{departure.headsign}</> : null}
+                                </div>
+                            </div>
+                            <div style={{ float: "right", textAlign: "right" }}>
+                                {new Date(departure.realTime).toLocaleTimeString()}
+                            </div>
+                        </ListItemText>
+                    </ListItem>
+                )).reduce((prev, curr) => [prev, <Divider variant="inset" component="li" key={Math.random()} sx={{ backgroundColor: "#DCCDCD", marginLeft: "15px", marginRight: "15px" }} />, curr]) || <h1 style={{ textAlign: "center" }}>Ładowanie</h1>}
+            </List>
         </BottomSheet>
     </>;
 };
