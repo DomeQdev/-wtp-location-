9f74324a1c2ed9be0ba3bd37d48cd349ac08fd7e
DomeQdev <domeqdev@pm.me>
2022-04-11 17:04:04 +0200

    Update filterData.js

--- functions/filterData.js
+++ functions/filterData.js
@@ -1,7 +1,7 @@
 export const onRequestGet = async ({ request }) => {
     let data = await Promise.all([
-        fetch("https://new-api.domeqalt.repl.co/all/routes", { cf: { cacheTtl: 86400, cacheEverything: true }, keepalive: true }).then(res => res.json()),
-        fetch("https://new-api.domeqalt.repl.co/all/filter", { cf: { cacheTtl: 86400, cacheEverything: true }, keepalive: true }).then(res => res.json())
+        fetch("https://old-api.matfiu.repl.co/all/routes", { cf: { cacheTtl: 86400, cacheEverything: true }, keepalive: true }).then(res => res.json()),
+        fetch("https://old-api.matfiu.repl.co/all/filter", { cf: { cacheTtl: 86400, cacheEverything: true }, keepalive: true }).then(res => res.json())
     ]);
     return new Response(JSON.stringify({
         routes: data[0],
