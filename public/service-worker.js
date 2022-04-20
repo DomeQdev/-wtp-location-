0cd637843957f595a17f8a4957f7bfd9bfc626d4
DomeQdev <domeqdev@pm.me>
2022-04-20 18:00:48 +0200

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,24 @@
-self.skipWaiting();self.addEventListener("fetch", () => {});
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open("zbiorkom").then(cache => {
+      return cache.addAll([
+        "/", "/index.html", "/manifest.json"
+      ])
+          .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open("zbiorkom")
+      .then(cache => cache.match(event.request, {ignoreSearch: true}))
+      .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
